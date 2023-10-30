79909a4b25d9659643dc97037dd14b19b598e272
Robin <87436238+epi55@users.noreply.github.com>
2023-10-30 10:10:05 -0700

    Update statement-scraper.py

--- statement-scraper.py
+++ statement-scraper.py
@@ -1,69 +1,86 @@
 import os
+import re
+import nltk
+from nltk.corpus import stopwords
 import pandas as pd
 import numpy as np
 from openpyxl.workbook import Workbook
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 # ENGINE
 def runEngine():
     statementFolder = r"Money management\Statements"
     outputFolder = r"Money management\Outputs"
 
-    allData = pd.DataFrame(columns=['date', 'vendor', 'debit', 'credit', 'bank', 'card'])
+    allData = pd.DataFrame(columns=['date', 'vendor', 'debit', 'credit', 'bank', 'drop1'])
 
     for filename in os.listdir(statementFolder):
         documentPath = os.path.join(statementFolder, filename)
-        data = extractEngine(filename, documentPath)
-        allData = pd.concat([allData, data], ignore_index=True)
+        if os.path.splitext(documentPath)[1] == '.csv':
+            data = extractEngine(filename, documentPath)
+            allData = pd.concat([allData, data], ignore_index=True)
+        else:
+            print("Error: \"{}\" is not a CSV file and will be skipped.".format(filename))
 
+    categorizerEngine(allData)
     outputEngine(allData, outputFolder)
 
 def extractEngine(filename, documentPath):
-    if filename.startswith(("cibc")):
-        # Column names NOT PROVIDED; added
-        df = pd.read_csv(documentPath, header=None, names=['date', 'vendor', 'debit', 'credit', 'cc number'])
+    
+    def migrateCredits(credit):
+        return credit if credit > 0 else None
 
+    def cleanDates(df):
+        for index, row in df.iterrows():
+            date = pd.to_datetime(row['date'], format='%m/%d/%y', errors='coerce')
+            if not pd.isna(date):
+                df.at[index, 'date'] = date.strftime('%Y-%m-%d')
+
+    def filenameToCols(filename):
+        drop1List = filename.split(' ')
+        df['bank'] = drop1List[0]
+        df['drop1'] = drop1List[1] + ' ' + drop1List[2]
+
+    # NOTE
+    # Code unique to CIBC CSV files
+    # Adjustments made to standardize data structure for cf.concat
+    if filename.startswith(("cibc")):
+        # COL headers NOT provided in CSV 
+        df = pd.read_csv(documentPath, header=None, names=['date', 'vendor', 'debit', 'credit', 'cc number'])
         df.drop(columns=['cc number'], inplace=True)
 
-        # TODO Automate based on filename (need to standardize)
-        cardList = filename.split(' ')
-        df['bank'] = cardList[0]
-        df['card'] = cardList[1] + ' ' + cardList[2]
-
+        filenameToCols(filename)
+        cleanDates(df)
         return df
 
-    if filename.startswith(("rbc")):
-        # Column names PROVIDED; removed then added
-        df = pd.read_csv(documentPath, skiprows=1, header=None, names=['card', 'account type', 'date', 'b1', 'vendor', 'b2', 'debit', 'b3', 'b4'])
-        
-        # DATA STD: CREDIT + DEBIT
-        df.drop(columns=['card', "account type", "b1", "b2", "b3", "b4"], inplace=True)
+    # NOTE
+    # Code unique to RBC CSV files
+    # Adjustments made to standardize data structure for cf.concat
+    elif filename.startswith(("rbc")):
+        # COL headers provided in CSV
+        df = pd.read_csv(documentPath, skiprows=1, header=None, names=['drop1', 'drop2', 'date', 'drop3', 'vendor', 'drop4', 'debit', 'drop5', 'drop6'])
+        df.drop(columns=['drop1', "drop2", "drop3", "drop4", "drop5", "drop6"], inplace=True)
         df['credit'] = df['debit'].apply(migrateCredits)
         df['debit'] = df['debit'].apply(abs)
         for index, row in df.iterrows():
             if row['debit'] == row['credit']:
                 df.at[index, 'debit'] = float('nan')
 
-        # DATA STD: DATE
-        #for index, row in df.iterrows():
-        #    df.at[index, 'date'] = pd.to_datetime(row['date'], format='%m/%d/%Y').strftime('%Y-%m-%d')
-
-        # TODO Automate based on filename (need to standardize)
-        cardList = filename.split(' ')
-        df['bank'] = cardList[0]
-        df['card'] = cardList[1] + ' ' + cardList[2]
-
+        filenameToCols(filename)
+        cleanDates(df)
         return df
-
-def migrateCredits(credit):
-    return credit if credit > 0 else None
+    
+    else:
+        print("##########\n# ERROR\n# \"{}\" is not a recognized bank.\n# Check filename or add additional code to parse file.\n##########".format(filename))
 
 def categorizerEngine(df):
     
-    def preprocess(text):
-        text = text.lower()
-        text = re.sub(r'[^\w\s]', '', text)
+    def preprocess(vendorDesc):
+        vendorDesc = vendorDesc.lower()
+        vendorDesc = re.sub(r'[^\w\s]', '', vendorDesc)
         stop_words = set(nltk.corpus.stopwords.words('english'))
-        tokens = nltk.word_tokenize(text)
+        tokens = nltk.word_tokenize(vendorDesc)
         tokens = [token for token in tokens if token not in stop_words]
         stemmer = nltk.stem.PorterStemmer()
         tokens = [stemmer.stem(token) for token in tokens]
@@ -72,9 +89,9 @@ def categorizerEngine(df):
 
     df['vendor'] = df['vendor'].apply(preprocess)
 
-    # Extract features from the preprocessed text data using bag-of-words model
+    # Extract features from the preprocessed vendorDesc data using bag-of-words model
     vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(df['Description'])
+    X = vectorizer.fit_transform(df['vendor'])
 
     # Train a Naive Bayes classifier on the extracted features
     y = df['Category']
@@ -83,7 +100,7 @@ def categorizerEngine(df):
 
     # Predict categories for new transactions
     new_transactions = ['AMAZON.COM*ajlja09ja', 'Shell Oil 4106541031']
-    new_transactions_preprocessed = [preprocess(text) for text in new_transactions]
+    new_transactions_preprocessed = [preprocess(vendorDesc) for vendorDesc in new_transactions]
     X_new = vectorizer.transform(new_transactions_preprocessed)
     y_new = clf.predict(X_new)
 
