45ae3a8fdb7326714933917ae45da047b1f4d141
Robin <87436238+epi55@users.noreply.github.com>
2023-12-30 13:38:16 -0500

    Add files via upload

--- categoryTrainer.py
+++ categoryTrainer.py
@@ -12,26 +12,28 @@ def setup_engine(output_file, reference_file):
     df_output_file = pd.read_csv(output_file, skiprows=1, header=None, usecols=[0,1,2,3,4,5,6,7,8,9], names=['date1', 'date2', 'vendor', 'debit', 'credit', 'bank', 'account', 'categoryAuto', 'categoryManual', 'person'])
     df_reference_file = pd.read_csv(reference_file)
 
-    # NOTE: ITERATE OUTPUT FILE
+    print(df_reference_file)
+
     for index, row in df_output_file.iterrows():
-        # NOTE: CLEAN CATEGORY COLUMN
+        # NOTE: CLEAN CATEGORY AND VENDOR COLUMNS
         if pd.isna(df_output_file.at[index, 'categoryAuto']) == False:
             category_preclean = df_output_file.at[index, 'categoryAuto']
             category_postclean = re.sub(r'(?<=\w) +(?=\w)|(?<=\w) +$|^ +| +(?=\w)', '', category_preclean).lower()
 
             vendor_preclean = df_output_file.at[index, 'vendor']
-            vendor_postclean = category_postclean = re.sub(r'(?<=\w) +(?=\w)|(?<=\w) +$|^ +| +(?=\w)', '', vendor_preclean).lower()
-            
-            if category_postclean in df_reference_file.columns:
-                if vendor_postclean not in df_reference_file[category_postclean].values:
-                    df_reference_file[category_postclean] = vendor_postclean
+            vendor_postclean = re.sub(r'(?<=\w) +(?=\w)|(?<=\w) +$|^ +| +(?=\w)', '', vendor_preclean).lower()
+
+            if category_postclean in df_reference_file['Category'].values:
+                if vendor_postclean in df_reference_file.loc[df_reference_file['Category'] == category_postclean, 'Vendor'].values[0]:
+                    pass
+                else:
+                    df_reference_file.loc[df_reference_file['Category'] == category_postclean, 'Vendor'] += [[vendor_postclean]]
             else:
-                df_reference_file[category_postclean] = vendor_postclean
-   
-    df_reference_file.to_csv(reference_file, index=False)
-  
-            ## IF CATEGORY EXISTS BUT IS IDENTIFIED AS UNIQUE (i.e. accompanied with *)
-            ## apply primary category but prompt input() for secondary category
+                df_reference_file = pd.concat([df_reference_file, pd.DataFrame([{'Category': category_postclean, 'Vendor': [[vendor_postclean]]}])], ignore_index=True)
+
+    print(df_reference_file)
+
+    # df_reference_file.to_csv(reference_file, index=False)
 
 # RUN
 ## TODO: Can these be done together? output_folder and output_file, etc.
