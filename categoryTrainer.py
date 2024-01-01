084e521e7c64f67a3af5d8804e220912fb92e1cb
Robin <87436238+epi55@users.noreply.github.com>
2024-01-01 16:54:12 -0500

    Add files via upload

--- categoryTrainer.py
+++ categoryTrainer.py
@@ -9,39 +9,33 @@ import os
 def setup_engine(output_file, reference_file):
 
     # NOTE: LOAD OUTPUT AND REFERENCE FILES
-    df_output_file = pd.read_csv(output_file, skiprows=1, header=None, usecols=[0,1,2,3,4,5,6,7,8,9], names=['date1', 'date2', 'vendor', 'debit', 'credit', 'bank', 'account', 'categoryAuto', 'categoryManual', 'person'])
-    df_reference_file = pd.read_csv(reference_file)
+    df_out = pd.read_csv(output_file, skiprows=1, header=None, usecols=[0,1,2,3,4,5,6,7,8,9], names=['date1', 'date2', 'vendor', 'debit', 'credit', 'bank', 'account', 'categoryAuto', 'categoryManual', 'person'])
+    df_ref = pd.read_csv(reference_file)
 
-    print(df_reference_file)
+    print(df_ref)
 
-    for index, row in df_output_file.iterrows():
+    for index, row in df_out.iterrows():
         # NOTE: CLEAN CATEGORY AND VENDOR COLUMNS
         # TODO: Need to make sure not all spaces are removed via regex
         # TODO: Remove *; * can be used later to identify unique/non-unique categorizations
-        if pd.isna(df_output_file.at[index, 'categoryAuto']) == False:
-            category_preclean = df_output_file.at[index, 'categoryAuto']
+        if pd.isna(df_out.at[index, 'categoryAuto']) == False:
+            category_preclean = df_out.at[index, 'categoryAuto']
             category = re.sub(r'(?<=\w) +(?=\w)|(?<=\w) +$|^ +| +(?=\w)', '', category_preclean).lower()
 
-            vendor_preclean = df_output_file.at[index, 'vendor']
+            vendor_preclean = df_out.at[index, 'vendor']
             vendor = re.sub(r'(?<=\w) +(?=\w)|(?<=\w) +$|^ +| +(?=\w)', '', vendor_preclean).lower()
 
-            if category in df_reference_file['Category'].values:
-                if vendor not in df_reference_file.loc[df_reference_file['Category'] == category, 'Vendor'].values[0]:
-                    print(type(df_reference_file.loc[df_reference_file['Category'] == category, 'Vendor']).values)
+            if category in df_ref['Category'].values:
+                if vendor not in df_ref.loc[df_ref['Category'] == category, 'Vendor'].values:
+                    df_ref.loc[df_ref['Category'] == category, 'Vendor'] += vendor
             else:
-                df_reference_file = pd.concat([df_reference_file, pd.DataFrame([{'Category': category, 'Vendor': vendor}])], ignore_index=True)
+                df_ref = pd.concat([df_ref, pd.DataFrame([{'Category': category, 'Vendor': vendor}])], ignore_index=True)
 
 
-            # if category in df_reference_file['Category'].values:
-            #     if vendor not in df_reference_file.loc[df_reference_file['Category'] == category, 'Vendor'].values[0]:
-            #         df_reference_file.loc[df_reference_file['Category'] == category, 'Vendor'].values[0].append(vendor)
-            # else:
-            #     df_reference_file = pd.concat([df_reference_file, pd.DataFrame([{'Category': category, 'Vendor': vendor}])], ignore_index=True)
+    print(df_ref)
+    # print(df_ref.loc[df_ref['Category'] == 'transfer', 'Vendor'].values[0])
 
-    print(df_reference_file)
-    # print(df_reference_file.loc[df_reference_file['Category'] == 'transfer', 'Vendor'].values[0])
-
-    # df_reference_file.to_csv(reference_file, index=False)
+    # df_ref.to_csv(reference_file, index=False)
 
 # RUN
 ## TODO: Can these be done together? output_folder and output_file, etc.
