20aa51c2795fb6523958bbd353feea0df699518f
Robin <87436238+epi55@users.noreply.github.com>
2024-01-02 12:52:28 -0500

    Add files via upload

--- categoryTrainer.py
+++ categoryTrainer.py
@@ -12,30 +12,27 @@ def setup_engine(output_file, reference_file):
     df_out = pd.read_csv(output_file, skiprows=1, header=None, usecols=[0,1,2,3,4,5,6,7,8,9], names=['date1', 'date2', 'vendor', 'debit', 'credit', 'bank', 'account', 'categoryAuto', 'categoryManual', 'person'])
     df_ref = pd.read_csv(reference_file)
 
-    print(df_ref)
-
     for index, row in df_out.iterrows():
         # NOTE: CLEAN CATEGORY AND VENDOR COLUMNS
-        # TODO: Need to make sure not all spaces are removed via regex
-        # TODO: Remove *; * can be used later to identify unique/non-unique categorizations
         if pd.isna(df_out.at[index, 'categoryAuto']) == False:
+            pattern = r'\W' # Any one non-word character
+            pattern2 = r'\s+' # Any one (or more) spaces
+            
             category_preclean = df_out.at[index, 'categoryAuto']
-            category = re.sub(r'(?<=\w) +(?=\w)|(?<=\w) +$|^ +| +(?=\w)', '', category_preclean).lower()
-
+            category = re.sub(pattern, ' ', category_preclean).lower()
+            category = re.sub(pattern2, ' ', category)
+            
             vendor_preclean = df_out.at[index, 'vendor']
-            vendor = re.sub(r'(?<=\w) +(?=\w)|(?<=\w) +$|^ +| +(?=\w)', '', vendor_preclean).lower()
+            vendor = re.sub(pattern, ' ', vendor_preclean).lower()
+            vendor = re.sub(pattern2, ' ', vendor)
 
-            if category in df_ref['Category'].values:
-                if vendor not in df_ref.loc[df_ref['Category'] == category, 'Vendor'].values:
-                    df_ref.loc[df_ref['Category'] == category, 'Vendor'] += vendor
-            else:
-                df_ref = pd.concat([df_ref, pd.DataFrame([{'Category': category, 'Vendor': vendor}])], ignore_index=True)
+            df_ref.loc[len(df_ref)] = [category, vendor] # Add a new row with len()
 
-
-    print(df_ref)
-    # print(df_ref.loc[df_ref['Category'] == 'transfer', 'Vendor'].values[0])
-
-    # df_ref.to_csv(reference_file, index=False)
+    df_ref_grouped = df_ref.groupby('Category')['Vendor'].apply(lambda x: set(x)).reset_index() # Makes a set, managing duplicates
+    # df_ref_grouped = df_ref.groupby('Category')['Vendor'].agg(list).reset_index() # Makes a list, not managing duplicates
+    df_ref_grouped.to_csv(reference_file, index=False)
+    # print(df_ref_grouped) # TEST
+    print("Complete.")
 
 # RUN
 ## TODO: Can these be done together? output_folder and output_file, etc.
